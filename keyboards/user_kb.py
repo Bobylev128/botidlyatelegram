c714fc7aefa66e707b30bcdfb4f982fec1433d4b
Bobylev128 <167031263+Bobylev128@users.noreply.github.com>
2026-03-23 17:27:40 +0300

    Delete keyboards directory

--- keyboards/user_kb.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-
-def join_kb(link):
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="📩 Участвовать", url=link)]
-    ])
