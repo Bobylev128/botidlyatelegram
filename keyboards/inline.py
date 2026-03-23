7655884dac63e84b8164d9db60257a6088c19125
Bobylev128 <167031263+Bobylev128@users.noreply.github.com>
2026-03-23 16:17:38 +0300

    Delete keyboards directory

--- keyboards/inline.py
+++
@@ -1,7 +0,0 @@
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
-def vote_kb(gid, users):
-    return InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text=u, callback_data=f"vote:{gid}:{u}")]
-        for u in users
-    ])
