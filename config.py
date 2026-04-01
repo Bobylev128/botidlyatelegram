f927dba381d38a80fed7d955a131ec8df21ed1c4
Bobylev128 <167031263+Bobylev128@users.noreply.github.com>
2026-04-01 17:53:43 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,10 +6,10 @@ load_dotenv()
 TOKEN_API = os.getenv("API_TOKEN")
 PROVIDER_TOKEN = os.getenv("provider_token")
 
-MAIN_CHANNEL_ID = int(os.getenv("mainch"))
-ANNOUNCE_CHANNEL_ID = int(os.getenv("annch"))
+MAIN_CHANNEL_ID = int(os.getenv("mchid"))
+ANNOUNCE_CHANNEL_ID = int(os.getenv("achid"))
 
 BOT_LINK = os.getenv("botlink")
-CHANNEL_LINK = os.getenv("clink")
+CHANNEL_LINK = os.getenv("chlink")
 
 ADMIN_ID = int(os.getenv("admid"))
