076c8dd37daad4e7dc9eb32fda5d350e07492814
Bobylev128 <167031263+Bobylev128@users.noreply.github.com>
2026-04-01 17:33:56 +0300

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-TOKEN_API = os.getenv("API_TOKEN")
-MAIN_CHANNEL_ID = int(os.getenv("mchid"))
-ANNOUNCE_CHANNEL_ID = int(os.getenv("achid"))
-CHANNEL_LINK = os.getenv("chlink")
-ADMIN_ID = int(os.getenv("admid"))
-BOT_USERNAME = os.getenv("buser")
